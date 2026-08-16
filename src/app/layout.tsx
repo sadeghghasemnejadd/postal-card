4a308c65fcf84eae28005933309d425ff4406bac
sadeghghasemnejadd <ghasemnejad.sadegh@gmail.com>
2026-08-16 10:14:47 +0330

    feat: update color scheme to 'only light' in layout and globals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "دعوت‌نامه عقد فاطمه و صادق",
   description: "دعوت‌نامه مراسم عقد فاطمه و صادق",
+};
+
+export const viewport: Viewport = {
+  colorScheme: "only light",
+  themeColor: "#f9f0e0",
 };
 
 export default function RootLayout({
